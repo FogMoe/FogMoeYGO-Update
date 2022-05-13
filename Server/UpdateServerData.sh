d7b7852f49c90f16072780fcee2640aa107074b4
Kc <kc@fog.moe>
2022-05-13 09:18:04 +0800

    Update UpdateServerData.sh

--- Server/UpdateServerData.sh
+++ Server/UpdateServerData.sh
@@ -2,7 +2,7 @@
 
 set -e
 sudo su
-cd ygoserver/ygopro
+cd /home/ubuntu/ygoserver/ygopro
 rm -rf ygopro-FogMoe-card-database
 git clone -b without-pics https://api.mtr.pub/scarletkc/ygopro-FogMoe-card-database.git
 cp -rf ygopro-FogMoe-card-database/* ./ && rm -r ygopro-FogMoe-card-database
