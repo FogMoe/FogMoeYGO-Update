b8b3bcd258440047b91cf70dcab5d98733198279
Kc <kc@fog.moe>
2022-05-13 15:28:15 +0800

    Update UpdateServerCardData.sh

--- Server/UpdateServerCardData.sh
+++ Server/UpdateServerCardData.sh
@@ -6,5 +6,6 @@ rm -rf ygopro-FogMoe-card-database
 git clone -b without-pics https://api.mtr.pub/scarletkc/ygopro-FogMoe-card-database.git
 cp -rf ygopro-FogMoe-card-database/* ./ && rm -r ygopro-FogMoe-card-database
 mv FogMoe-cards.cdb cards.cdb
+pm2 restart 0
 
 echo "Update complete!"
