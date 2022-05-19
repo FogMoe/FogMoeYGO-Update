17c43ca8d9c0d06f599096919b5079a46d802756
Kc <kc@fog.moe>
2022-05-20 00:46:31 +0800

    Update UpdateServerCardData.sh

--- Server/UpdateServerCardData.sh
+++ Server/UpdateServerCardData.sh
@@ -3,7 +3,7 @@
 set -e
 cd /home/ubuntu/ygoserver/ygopro
 rm -rf ygopro-FogMoe-card-database
-git clone -b without-pics https://ghproxy.futils.com/https://github.com/scarletkc/ygopro-FogMoe-card-database.git
+git clone -b without-pics https://api.mtr.pub/scarletkc/ygopro-FogMoe-card-database.git
 cp -rf ygopro-FogMoe-card-database/* ./ && rm -r ygopro-FogMoe-card-database
 mv FogMoe-cards.cdb cards.cdb
 pm2 restart 0
